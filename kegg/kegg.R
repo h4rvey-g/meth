7d3bbaf1f50c9c1e8611c35e58731777da35e557
babaolanqiu <babaolanqiu@gmail.com>
2022-08-19 06:34:37 +0000

    kegg demo

---
+++ kegg/kegg.R
@@ -0,0 +1,17 @@
+library(clusterProfiler)
+library(tidyverse)
+library(readxl)
+library(DOSE)
+library(ggh4x)
+dat <- read_xlsx("./KEGG enrichment.xlsx") %>%
+  mutate(
+    GeneRatio = parse_ratio(GeneRatio)
+  ) %>%
+  arrange(level)
+p <- ggplot(dat, aes(x = GeneRatio, y = interaction(Level_1, Level_2, Level_3, sep = "&"))) +
+  geom_point(aes(size = Count, color = -log10(pvalue))) +
+  guides(
+    y = guide_axis_nested(delim = "&")
+  ) +
+  theme_classic()
+ggsave("./KEGG enrichment.pdf", p, width = 25, height = 10)
