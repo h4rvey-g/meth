b485288b87b2adb86ce3b4f5b88e946a5fc82393
babaolanqiu <babaolanqiu@gmail.com>
2022-08-19 07:18:05 +0000

    fix

--- kegg/kegg.R
+++ kegg/kegg.R
@@ -15,7 +15,7 @@ p <- ggplot(dat, aes(x = GeneRatio, y = interaction(Level_3, Level_2, Level_1, s
     y = guide_axis_nested(delim = "&")
   ) +
   labs(y = "Levels") +
-  theme_classic()
+  theme_few()
 ggsave("./KEGG enrichment.pdf", p, width = 25, height = 10)
 p2 <- ggplot(dat, aes(x = GeneRatio, y = interaction(Level_3, Level_2, Level_1, sep = "&"))) +
   geom_point(aes(size = Count, color = -log10(pvalue))) +
@@ -23,6 +23,6 @@ p2 <- ggplot(dat, aes(x = GeneRatio, y = interaction(Level_3, Level_2, Level_1, 
     y = guide_axis_nested(delim = "&")
   ) +
   labs(y = "Levels") +
-  theme_few()+
-  facet_grid(rows = vars(`Group comparisons`),scales = "free")
+  theme_few() +
+  facet_grid(rows = vars(`Group comparisons`), scales = "free")
 ggsave("./KEGG enrichment seprated.pdf", p2, width = 25, height = 10)
